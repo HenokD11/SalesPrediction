96c1815180293a7695f8b803d3f1fd46f1d68907
ahenokD1 <henokdes1@gmail.com>
2022-09-07 06:29:09 +0300

    error fix

--- scripts/data_manipulation.py
+++ scripts/data_manipulation.py
@@ -87,7 +87,7 @@ class DataManipulator:
         return new_index
 
 
-     def add_number_of_days_to_holiday(self, state_holiday_col: str):
+    def add_number_of_days_to_holiday(self, state_holiday_col: str):
         try:
             date_index = self.df.columns.get_loc(state_holiday_col)
 
@@ -259,3 +259,136 @@ class DataManipulator:
 
         except:
             print("Failed to scale the column")
+
+    def normalize_column(self, column: str) -> pd.DataFrame:
+        """
+            Returns the objects DataFrames column normalized using Normalizer
+            Parameters
+            ----------
+            column:
+                Type: str
+            length:
+                Type: int
+            Returns
+            -------
+            pd.DataFrame
+        """
+        try:
+            scale_column_df = pd.DataFrame(self.df[column])
+            scale_column_values = scale_column_df.values
+            normalizer = Normalizer()
+            normalized_data = normalizer.fit_transform(scale_column_values)
+            self.df[column] = normalized_data
+
+            return self.df
+
+        except:
+            print("Failed to normalize the column")
+
+    def standardize_column(self, column: str) -> pd.DataFrame:
+        """
+            Returns the objects DataFrames column normalized using Normalizer
+            Parameters
+            ----------
+            column:
+                Type: str
+            length:
+                Type: int
+            Returns
+            -------
+            pd.DataFrame
+        """
+        try:
+            std_column_df = pd.DataFrame(self.df[column])
+            std_column_values = std_column_df.values
+            standardizer = StandardScaler()
+            normalized_data = standardizer.fit_transform(std_column_values)
+            self.df[column] = normalized_data
+
+            return self.df
+        except:
+            print("Failed to standardize the column")
+
+    def standardize_columns(self, columns: list) -> pd.DataFrame:
+        try:
+            for col in columns:
+                self.df = self.standardize_column(col)
+
+            return self.df
+        except:
+            print(f"Failed to standardize {col} column")
+
+    def minmax_scale_column(self, column: str, range_tup: tuple = (0, 1)) -> pd.DataFrame:
+        """
+            Returns the objects DataFrames column normalized using Normalizer
+            Parameters
+            ----------
+            column:
+                Type: str
+            length:
+                Type: int
+            Returns
+            -------
+            pd.DataFrame
+        """
+        try:
+            std_column_df = pd.DataFrame(self.df[column])
+            std_column_values = std_column_df.values
+            minmax_scaler = MinMaxScaler(feature_range=range_tup)
+            normalized_data = minmax_scaler.fit_transform(std_column_values)
+            self.df[column] = normalized_data
+
+            return self.df
+        except:
+            print("Failed to standardize the column")
+
+    def minmax_scale_columns(self, columns: list, range_tup: tuple = (0, 1)) -> pd.DataFrame:
+        try:
+            for col in columns:
+                self.df = self.minmax_scale_column(col, range_tup)
+
+            return self.df
+        except:
+            print(f"Failed to MinMax standardize {col} column")
+
+    def fill_columns_with_max(self, columns: list) -> None:
+        try:
+            for col in columns:
+                self.df[col] = self.df[col].fillna(self.df[col].max())
+
+        except Exception as e:
+            print("Failed to fill with max value")
+
+    def fill_columns_with_most_frequent(self, columns: list) -> None:
+        try:
+            for col in columns:
+                self.df[col] = self.df[col].fillna(self.df[col].mode().iloc[0])
+
+        except Exception as e:
+            print("Failed to fill with max value")
+
+    def label_columns(self, columns: list) -> dict:
+        labelers = {}
+        try:
+            for col in columns:
+                le = LabelEncoder()
+                le_fitted = le.fit(self.df[col].values)
+                self.df[col] = le_fitted.transform(self.df[col].values)
+                labelers[col] = le_fitted
+
+            return labelers
+
+        except Exception as e:
+            print("Failed to Label Encode columns")
+
+    def create_date(self, name:str='Date',columns: list=['Year','Month','Day']) -> None:
+        date_column = []
+        try:
+            for index, row in self.df.iterrows():
+                date_column.append(
+                    datetime(int(row[columns[0]]), int(row[columns[1]]), int(row[columns[2]])))
+
+            self.df[name] = date_column
+        
+        except Exception as e:
+            print('Failed to create Date column',e)
